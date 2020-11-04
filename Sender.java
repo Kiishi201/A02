4b922f8782c1a5454d31830e0c3b91f231a07ff5
rollierivera <72164424+rollierivera@users.noreply.github.com>
2020-11-04 16:17:04 -0400

    Update Sender.java

--- Sender.java
+++ Sender.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -8,6 +9,7 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Sender {
 	//DatagramSocket socket = null;
@@ -47,22 +49,23 @@ public class Sender {
 		int packetCount=0; 
 		String alternate;
 		byte[] buf = new byte[(int) (long) datagramMaxSize];
-		 while (packetCount<=fileArray.length) {
+		DatagramPacket lastPacket = null;
+		boolean eof=false;
+		 while (packetCount<=fileArray.length&&!eof) {
 			 
 			 alternate= packetCount%2==0 ? "0": "1";
              // receive request
-             
+			 
              DatagramPacket packet = new DatagramPacket(buf, buf.length);
-             DatagramPacket lastPacket = null;
+            
 			 while(true) {
+				 
 				 try {
-					 socket.receive(packet);
-					 
+						 socket.receive(packet);
 					   // rest of the success path
 					} catch (SocketTimeoutException ex) {
 						System.err.print("Timeout");
 						socket.send(lastPacket);
-						
 					}
 				 
 				 String packetData = new String(packet.getData());
@@ -72,36 +75,43 @@ public class Sender {
 				 if(packetData.substring(0, 1).equals(alternate)) {
 					 System.out.println("Breaking");
 					 break;
+				 }else if(packetData.equals("EOF")) {
+					 eof=true;
+					 break;
 				 }
 				 
 			 }
-			 
-	            try {
-	                
-	                //socket.receive(packet);
+			 if(!eof) {
+				 try {
 
-	                // figure out response
-	                String dString = null;
-	                dString = fileArray[packetCount];
-	                packetCount++;
-	                buf = dString.getBytes();
+					 //socket.receive(packet);
 
-			// send the response to the client at "address" and "port"
-	                //InetAddress address = packet.getAddress();
-	                //int port = packet.getPort();
-	                
-	                InetAddress address = InetAddress.getByName(argv[0]); 
-	                //packet = new DatagramPacket(buf, buf.length, address, port);
-	                
-	                packet = new DatagramPacket(buf, buf.length, address, receiverPortNum);
-	                socket.send(packet);
-	                lastPacket=packet;
-	                socket.setSoTimeout(timeout/1000);
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	                
-	            }
+					 // figure out response
+					 String dString = null;
+					 dString = fileArray[packetCount];
+					 packetCount++;
+					 buf = dString.getBytes();
+
+					 // send the response to the client at "address" and "port"
+					 //InetAddress address = packet.getAddress();
+					 //int port = packet.getPort();
+
+					 InetAddress address = InetAddress.getByName(argv[0]); 
+					 //packet = new DatagramPacket(buf, buf.length, address, port);
+
+					 packet = new DatagramPacket(buf, buf.length, address, receiverPortNum);
+					 socket.send(packet);
+
+					 lastPacket=packet;
+					 //socket.setSoTimeout(timeout*1000);
+					 socket.setSoTimeout(timeout/1000);
+				 } catch (IOException e) {
+					 e.printStackTrace();
+
+				 }
+			 }
 	        }
+		 socket.close();
 		}else {
 			System.err.println("File is empty, Goodbye.");
 		}
@@ -118,6 +128,7 @@ public class Sender {
 		 */
 		
 	}
+	/*
 	public static String[] fileToArray() {
         String [] packetString;
         packetString= new String[(int) (Math.ceil( ((double)file.length()/datagramMaxSize)))+1];
@@ -126,6 +137,7 @@ public class Sender {
         int count=0;
         String seqNum;
         packetString[0]="0";
+        //Try to take this out, check if it still works
         for (int y=1;y<packetString.length;y++) {
         	packetString[y]="";
         }
@@ -153,6 +165,41 @@ public class Sender {
         return packetString;
         
     }
+    */
+	public static String[] fileToArray() throws IOException {
+		String fileString = "";
+		String line;
+		String [] packetString= new String[(int) (Math.ceil( ((double)file.length()/datagramMaxSize)))+1];
+		String seqNum;
+		int count=0;
+		while(fileEnd) {
+        	try {
+        		if ((line= in.readLine()) == null) {
+        			in.close();
+        			fileEnd = false;
+        		}else {
+        			fileString=fileString.concat(line);
+        		}
+        	}catch (IOException e) {
+            		//packetString = "IOException occured in server";
+            	}
+		}	
+	    ByteArrayInputStream bis = new ByteArrayInputStream(fileString.getBytes());
+	    int n = 0;
+	    byte[] buffer = new byte[(int) datagramMaxSize];
+	    String result = "";
+	    while ((n = bis.read(buffer)) > 0) {
+	    	seqNum= count%2==0 ? "0": "1";
+	    	result=seqNum;
+	        for (byte b : buffer) {
+	            result+= (char) b;
+	        }
+	        Arrays.fill(buffer, (byte) 0);
+	        packetString[count]=result;
+	        count++;
+	    }
+	    return packetString;
+	}
 public class Datagram{
 	private Integer sequenceNumber;
 	
